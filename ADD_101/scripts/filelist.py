7164533e87a419fe678a74d4dd235629d1f2ac02
ericdeutsch <edeutsch@syr.edu>
2020-09-25 02:24:11 -0400

    Completed exercises from Week 5

--- ADD_101/scripts/filelist.py
+++ ADD_101/scripts/filelist.py
@@ -5,7 +5,7 @@ import pandas as pd
 ftp_dir = "ftp://ftp2.psl.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/surface_gauss/"
 variable = "air.2m.gauss."
 file_type = ".nc"
-time =pd.Series(list(range(1950,1970)))
+time =pd.Series(list(range(1990,2011)))
 time = time.apply(str)
 filepaths= ftp_dir + variable + time + file_type
-filepaths.to_csv("../data/filelist.txt", index=False)
+filepaths.to_csv("../data/raw_data/filelist.txt", index=False)
